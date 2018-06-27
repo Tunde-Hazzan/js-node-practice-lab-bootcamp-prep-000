f4418864dabc868c0f167eb29add9948d6cbda5d
Tunde Hazzan <t.hazzan@ctrlmetrics.com>
2018-06-27 08:05:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 // write your code below!
 
 var name = "Tunde"
-var height = "180"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
-console.log((parseInt(height) + 1)
+console.log(height + 1)
 console.log(height)
